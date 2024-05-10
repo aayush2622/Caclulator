1af6514c2ed159506b179a817ffa8e72a95ed3eb
aayush262 <aayushthakur262006@gmail.com>
2024-05-10 23:39:26 +0530

    fix: Amoled theme

--- app/src/main/java/com/aayush/calculator/ThemeManager.kt
+++ app/src/main/java/com/aayush/calculator/ThemeManager.kt
@@ -2,6 +2,7 @@ package com.aayush.calculator
 
 import android.app.Activity
 import android.os.Build
+import android.view.Window
 import android.view.WindowManager
 import com.aayush.calculator.saving.PrefManager
 import com.aayush.calculator.saving.PrefName
@@ -11,20 +12,28 @@ import com.google.android.material.color.DynamicColorsOptions as MaterialUIOptio
 object ThemeManager {
 
     fun apply(context: Activity) {
-        val window = context.window
+        setWindowFlag(context.window)
+
+        val useAmoled: Boolean = PrefManager.getVal(PrefName.Amoled)
+
+        val theme = MaterialUIOptions.Builder().build()
+        MaterialUI.applyToActivityIfAvailable(context, theme)
+
+        if (useAmoled) {
+            val themeAmoled = MaterialUIOptions.Builder() // apply on top of the current theme
+                .setThemeOverlay(R.style.AppTheme_Amoled)
+                .build()
+            MaterialUI.applyToActivityIfAvailable(context, themeAmoled)
+        }
+    }
+
+    private fun setWindowFlag(window: Window) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
             @Suppress("DEPRECATION")
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = 0x00000000
-
-        val useAmoled: Boolean = true
-
-        val theme = MaterialUIOptions.Builder()
-            .setThemeOverlay(if (useAmoled) R.style.AppTheme_Amoled else 0)
-            .build()
-
-        MaterialUI.applyToActivityIfAvailable(context, theme)
+        window.navigationBarColor = 0x00000000
     }
 }
